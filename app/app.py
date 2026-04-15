54bff20f3175f3e5edb21d7513b0fb430dbfa86d
Kirito-C <149937627+Kirito-C@users.noreply.github.com>
2026-04-16 00:38:43 +0200

    app.py e compose
    aggiunto il nome al compose e terminata la webapp di base.

--- app/app.py
+++ app/app.py
@@ -1,11 +1,28 @@
-from flask import Flask
+import json
+
+from flask import Flask, request, jsonify
 import paho.mqtt.client as mqtt
 
-def publish(data):
-    pass
+def handle_publish(data):
+    client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+    client.connect("emqx", 1883, 60)
+    client.publish("notifictions", json.dumps(data))
+    client.disconnect()
 
 if __name__ == "__main__":
     app = Flask(__name__)
 
-    @app.route("/")
-    
+    @app.post("/publish")
+    def publish():
+        data = request.get_json()
+        try:
+            handle_publish(data)
+            return jsonify({
+                "status": 200,
+                "message": "publish executed succesfully!"
+            })
+        except Exception as e:
+            return jsonify({
+                "status": 500,
+                "message": repr(e)
+            })
